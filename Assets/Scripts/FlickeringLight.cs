545acac9edbe23cb3186eff25d88257766eaea19
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-02-12 16:14:15 +0100

    fuck off

---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+    private Light myLight;
+    void Start () {
+        myLight = GetComponent<Light>();
+    }
+	
+	void Update () 
+	{
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        if (Random.Range(0, 800) == 3)
+        {
+            myLight.enabled = !myLight.enabled;
+            GetComponent<AudioSource>().mute = false;
+            GetComponent<AudioSource>().volume = Random.Range(0.4f, 0.7f);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+            GetComponent<AudioSource>().Play();
+            yield return new WaitForSeconds(0.2f);
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Footsteps : MonoBehaviour {
+
+	CharacterController cc;
+	
+	void Start ()	
+ 	{
+		cc = GetComponent<CharacterController>();
+	}
+	
+	void Update ()	
+ 	{
+		if(cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+		{
+            GetComponent<AudioSource>().mute = false;
+            GetComponent<AudioSource>().volume = Random.Range(0.4f, 0.7f);
+			GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
