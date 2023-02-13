b36bdb900024d8b22e89aa69d1e97683b2609567
Ruben Zsidó <zsruben03@gmail.com>
2023-02-13 11:02:41 +0100

    Current Progress

--- Assets/BossHealth.cs
+++ Assets/BossHealth.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BossHealth : MonoBehaviour
 {
+    [SerializeField] FloatVariable currHealth;
+    [SerializeField] FloatVariable maxHealth;
+    [SerializeField] GameEvent BossDamaged;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,11 @@ public class BossHealth : MonoBehaviour
     void Update()
     {
         
+
+    }
+
+    public void OnDamaged()
+    {
+
     }
 }
